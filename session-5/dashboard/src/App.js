1790ee8aad2022987382036951e67e150704a6a0
mvo <mary.vokicic@starfinanz.de>
2020-05-27 17:41:55 +0200

    add dashboard

---
+++ session-5/dashboard/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section className="sidebar">
+        Sidebar
+        </section>
+      <section className="main">
+        Main....
+        </section>
+    </div>
+  );
+}
+
+export default App;
